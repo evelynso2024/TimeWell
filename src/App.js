da5d2df0f0db95e133887e92818874729bf5a93f
evelynso2024 <evelyn.so@thinkerandco.com>
2025-03-02 15:46:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/auth/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Timer from './components/Timer';
 import AllTasks from './components/AllTasks';
 import Summary from './components/Summary';
 import Insights from './components/Insights';
+import Login from './components/Login';
+import { supabase } from './supabaseClient';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/timer" element={<Timer />} />
-          <Route path="/alltasks" element={<AllTasks />} />
-          <Route path="/summary" element={<Summary />} />
-          <Route path="/insights" element={<Insights />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/timer" element={<Timer />} />
+        <Route path="/alltasks" element={<AllTasks />} />
+        <Route path="/summary" element={<Summary />} />
+        <Route path="/insights" element={<Insights />} />
+        <Route path="/" element={<Timer />} />
+      </Routes>
     </Router>
   );
 }
