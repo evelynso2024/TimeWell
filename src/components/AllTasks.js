ff1a3c4597376b3ce104206714bca814b00de7e7
evelynso2024 <evelyn.so@thinkerandco.com>
2025-03-02 13:13:25 -0500

    Update AllTasks.js

--- src/components/AllTasks.js
+++ src/components/AllTasks.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { db, auth } from '../firebase'; // Updated path with ../
 import { useEffect, useState } from 'react';
-import Navbar from './Navbar';
+import Navbar from '../Navbar';
 import { useNavigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
-
 
 
 
