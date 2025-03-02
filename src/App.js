a5b898de188526c948a7f2d873f7e1322d5a78ba
evelynso2024 <evelyn.so@thinkerandco.com>
2025-03-02 17:47:46 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Timer from './components/Timer';
 import AllTasks from './components/AllTasks';
 import Summary from './components/Summary';
 import Insights from './components/Insights';
 import Login from './components/auth/Login';
-import { supabase } from './supabaseClient';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Public routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        
-        {/* Protected routes */}
         <Route path="/timer" element={<Timer />} />
         <Route path="/alltasks" element={<AllTasks />} />
         <Route path="/summary" element={<Summary />} />
