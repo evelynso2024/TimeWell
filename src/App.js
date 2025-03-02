ee263be4ee27f7c275055868dc6ef185bfea9847
evelynso2024 <evelyn.so@thinkerandco.com>
2025-03-02 14:03:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './component/auth/Login';
-import Timer from './component/Timer';
-import AllTasks from './component/AllTasks';
+import Login from './components/auth/Login';
+import Timer from './components/Timer';
+import AllTasks from './components/AllTasks';
 
 function App() {
   return (
