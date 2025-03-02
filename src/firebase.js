f54f4b2f5be1562f86f1d33604153532603f068d
evelynso2024 <evelyn.so@thinkerandco.com>
2025-03-02 13:24:12 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,8 +8,8 @@ const firebaseConfig = {
   projectId: "timewell-c0b5f",
   storageBucket: "timewell-c0b5f.appspot.com",
   messagingSenderId: "1095371085844",
-  appId: "1:1095371085844:web:c0c5c2e2d6e0a5c0c9c9c9",
-  measurementId: "G-XXXXXXXXXX"  // Replace with your actual measurement ID if you have one
+  appId: "1:1095371085844:web:d4c5c2e2d6e0a5c0c9c9c9",
+  measurementId: "G-XXXXXXXXXX"
 };
 
 // Initialize Firebase
