633854869508e8c2524b6959257c050232997c59
evelynso2024 <evelyn.so@thinkerandco.com>
2025-03-02 15:51:52 -0500

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../../supabaseClient';
 
 function Summary() {
   const [user, setUser] = useState(null);
