f7e2c3965c3568dc9304201804ae95a568a65153
evelynso2024 <evelyn.so@thinkerandco.com>
2025-03-05 16:06:02 -0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hlrfjbgeezyoixwyosxj.supabase.co'  // Replace with your Supabase project URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhscmZqYmdlZXp5b2l4d3lvc3hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5MzgwMDgsImV4cCI6MjA1NjUxNDAwOH0.NIrwtDfK1K4-dbdVMl_vJobwsSV532La4zqnM5aw3YY' // Replace with your Supabase anon/public key
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
